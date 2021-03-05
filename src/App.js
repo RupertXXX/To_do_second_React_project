d8ab5b31e102091c7a701f1e99e1d06206c928d1
RupertXXX <70776356+RupertXXX@users.noreply.github.com>
2021-03-06 01:44:06 +0300

    Создал основную структуру и поудалял лишнее

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-import './App.css';
+import React from 'react';
+import Menu from './components/menu/menu';
+import AddNote from './components/addNote/addNote';
+import Notes from './components/notes/notes';
+import c from './App.module.css';
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className={c.App} >
+      <Menu />
+      <Notes />
+      <AddNote />
     </div>
   );
 }
