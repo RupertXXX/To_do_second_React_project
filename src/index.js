af74e00bf5e00e7700eb5feec8f39fbbd2592e61
RupertXXX <70776356+RupertXXX@users.noreply.github.com>
2021-03-08 23:05:24 +0300

    добавил HashRouter

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter} from 'react-router-dom';
+import {HashRouter} from 'react-router-dom';
 import { Provider } from 'react-redux';
 import './index.css';
 import store from './redux/redux';
@@ -9,9 +9,9 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
     <Provider store={store}>
-      <BrowserRouter>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </Provider>,
   document.getElementById('root')
 );
